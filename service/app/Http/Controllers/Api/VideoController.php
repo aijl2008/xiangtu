1eaf8993966cfda3940e5e82d1de8036f77203f6
aijl2008 <Jerry790928>
2018-12-12 11:38:35 +0800

    视频列表返回每个视频是否被当前用户关注

--- service/app/Http/Controllers/Api/VideoController.php
+++ service/app/Http/Controllers/Api/VideoController.php
@@ -16,12 +16,22 @@ class VideoController extends Controller
      */
     public function index(Request $request)
     {
+        if ($user = $request->user('api')) {
+            $user_id = $user->id;
+        } else {
+            $user_id = 0;
+        }
         return Helper::success(Video::query()
-            ->when($classification = $request->input('classification'), function (Builder $queries) use ($classification) {
+            ->when($classification = $request->input('classification'), function (Builder $queries) use ($classification, $user_id) {
                 return $queries->where('classification_id', $classification);
             })
             ->with('wechat')
+            ->withCount([
+                'liker' => function (Builder $query) use ($user_id) {
+                    return $query->where('wechat_id', $user_id);
+                }
+            ])
             ->orderBy('id', 'desc')
-            ->paginate(20));
+            ->paginate(10)->appends('classification', $classification));
     }
 }
--- service/app/Models/Video.php
+++ service/app/Models/Video.php
@@ -35,4 +35,8 @@ class Video extends Model
     {
         return $this->belongsTo(Wechat::class);
     }
+
+    function liker(){
+        return $this->belongsToMany(Wechat::class);
+    }
 }
