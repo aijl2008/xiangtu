9c22fa0523b4562158199d289b335034f35d11c0
aijl2008 <Jerry790928>
2019-01-12 23:22:34 +0800

    添加公众号

--- service/database/migrations/2019_01_12_221534_create_official_account_followers_table.php
+++ service/database/migrations/2019_01_12_221534_create_official_account_followers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFollowersTable extends Migration
+class CreateOfficialAccountFollowersTable extends Migration
 {
     /**
      * Run the migrations.
